dae4e8a86eae19629b672991a84272392bee62fe
vaibhav ghule <vaibhavghule7507@gmail.com>
2022-11-10 19:21:05 +0530

    navbar compleate
    only hd photo remaining

--- AlmabetterProject/src/App.js
+++ AlmabetterProject/src/App.js
@@ -1,24 +1,11 @@
-import logo from './logo.svg'
+import Navbar from './Component/Navbar'
 import './App.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> Rahul & Vaibhav code
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Navbar/>
+    </>
   )
 }
 
