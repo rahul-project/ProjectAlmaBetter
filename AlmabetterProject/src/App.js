335dc2e1ce7f14cf8adefa9abf203762c3e66ab9
Rahul Mule <108717153+rahul-project@users.noreply.github.com>
2022-11-19 10:44:41 +0530

    nav bar
    change

--- AlmabetterProject/src/App.js
+++ AlmabetterProject/src/App.js
@@ -1,11 +1,14 @@
 import Navbar from './Component/navbar/Navbar'
 import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import MainBar from './Components/SideBar/mainBar'
+
 
 function App() {
-  return (
-    <>
-      <Navbar />
-    </>
+  return (<><Navbar/>
+    <div className="App">
+      <MainBar />
+    </div></>
   )
 }
 
